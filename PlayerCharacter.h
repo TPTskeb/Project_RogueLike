f6c4ce149300d298de0d66ab90cef1eb470e8112
Botond "skeb" Péter-Szabó <156703309+TPTskeb@users.noreply.github.com>
2026-02-13 20:54:32 +0100

    Add files via upload

--- PlayerCharacter.h
+++ PlayerCharacter.h
@@ -2,7 +2,9 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputActionValue.h"
 #include "PlayerCharacter.generated.h"
+
 
 UCLASS()
 class PROJECT_ROUGELIKE_API APlayerCharacter : public ACharacter
@@ -16,6 +18,26 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	class UInputMappingContext* PlayerMappingContext;
+
+	//Input Action Move
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	class UInputAction* MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* LookupAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* TurnAction;
+
+	//Function to move any direction
+	void Move(const FInputActionValue& Value);
+
+	void Turn(const FInputActionValue& Value);
+	void LookUp(const FInputActionValue& Value);
+	
 
 public:	
 	// Called every frame
