f614e00c0e50f5c6504c2fd98393cf14d3f83f23
Botond "skeb" Péter-Szabó <156703309+TPTskeb@users.noreply.github.com>
2026-02-13 20:54:45 +0100

    Delete Project_Rougelike.cpp

--- Project_Rougelike.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Project_Rougelike.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project_Rougelike, "Project_Rougelike" );
